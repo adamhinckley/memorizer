376cf58415f9bf0144679775113a61f531e1d69a
Adam Hinckley <adam.hinckley@clickbank.com>
2023-09-03 10:54:42 -0500

    Update home page

--- app/page.js
+++ app/page.js
@@ -2,19 +2,18 @@ import Link from 'next/link';
 
 export default function Home() {
 	return (
-		// <nav className="">
-		// 	<li>
-		// 		<Link href="/make-my-own">Make My Own</Link>
-		// 	</li>
-		// 	<li>
-		// 		<Link href="/create-link">Create a link to share</Link>
-		// 	</li>
-		// </nav>
-		<div className="flex flex-col items-center justify-center min-h-screen py-2">
-			<nav className="flex flex-col items-center  ">
-				<Link href="/make-my-own">Make My Own</Link>
-				<Link href="/create-link">Create a link to share</Link>
-			</nav>
+		<div>
+			<p className="mt-4 text-center">
+				Welcome to Memorizer, the tool for memorizing text by making parts of it disappear.
+			</p>
+			<div className="flex flex-col items-center justify-center min-h-screen py-2">
+				<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+					<Link href="/make-my-own">Make My Own</Link>
+				</button>
+				<button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-4">
+					<Link href="/create-link">Create a link to share</Link>
+				</button>
+			</div>
 		</div>
 	);
 }
